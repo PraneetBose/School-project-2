c2f98e33f711cfb69331e43d87847d0c7e17d64f
Praneet Bose <94593299+PraneetBose@users.noreply.github.com>
2021-12-25 22:55:43 -0500

    Create project2.py

---
+++ project2.py
@@ -0,0 +1,13 @@
+# Python3 Program to
+# accept five number , find their sum and average
+
+#making 5 variables, namely -a ,b ,c ,d and e
+a=int(input("Enter the First number - "))
+b=int(input("Enter the Second number- "))
+c=int(input("Enter the Third number- "))
+d=int(input("Enter the Fourth number- "))
+e=int(input("Enter the Fifth number- "))
+#finding the Sum using the int stored in the 5 variables .
+print("The Sum of the given numbers are =  ",a+b+c+d+e)
+#finding the average of the int
+print("The Average of the given numbers are  = ",a+b+c+d+e/5)
